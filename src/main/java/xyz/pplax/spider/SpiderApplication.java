bc4104eb91f67a8b4cbc126e5ae6b691bffc7235
pplax <lax1458667357@gmail.com>
2024-02-13 18:37:39 +0800

    调整

--- src/main/java/xyz/pplax/spider/SpiderApplication.java
+++ src/main/java/xyz/pplax/spider/SpiderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpiderDemoApplication {
+public class SpiderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpiderDemoApplication.class, args);
+        SpringApplication.run(SpiderApplication.class, args);
     }
 
 }
---
+++ src/test/java/xyz/pplax/spider/SpiderApplicationTests.java
@@ -0,0 +1,9 @@
+package xyz.pplax.spider;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderApplicationTests {
+
+
+}
